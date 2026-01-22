499ca91951a689a6f8066c162de6caa768eaa85a
hmatenciov81-gif <hmatenciov81@gmail.com>
2026-01-22 08:23:27 -0500

    correccion url instalador drive

--- servidor_licencias_mongodb.js
+++ servidor_licencias_mongodb.js
@@ -1464,7 +1464,7 @@ app.get('/instalador', (req, res) => {
 
 // Descargar instalador desde Google Drive
 app.get('/api/instalador/download', (req, res) => {
-  const googleDriveId = '1ERo7swjBqPpMHRANuI2H4MFyLc4STUPF';
+  const googleDriveId = '1C7bO2DcMgEn1mLVOK0KYm4uLrNfFSZ3v';
   const url = `https://drive.usercontent.google.com/download?id=${googleDriveId}&export=download&confirm=t`;
   
   console.log('[INSTALADOR] Redirigiendo descarga a Google Drive');
